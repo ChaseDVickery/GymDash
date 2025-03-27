0d0c05397eb9889e249350e12eaad9e4cb70cd72
Chase Vickery <chasedvickery@gmail.com>
2025-03-27 16:42:02 -0400

    Test API Calls
    Add skeleton frontend to test API calls from webpage.
    Test JS script can stress test the FastAPI by changing
    number of requests. Also tested sending larger payloads
    with gzip compression. Zipping data tends to take longer
    than passing it locally, so minimum compression size is
    large (>=1MB).

--- src/api/api.py
+++ src/api/api.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import JSONResponse
 from random import randint
+import numpy as np
 
+
+# Setup our API
 app = FastAPI(title="GymDash", description="API for interacting with active simulation environments", version="0.0.1")
+
+# Setup CORS middleware so that our API will accept
+# communication from our frontend
+origins = [
+    # "*"
+    # "http://localhost",
+    # "http://localhost/*",
+    # "http://127.0.0.1:8000/random",
+    # "http://127.0.0.1*",
+]
+regex_origins = r"^((.*127.0.0.1.*)|(.*localhost.*))$"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_origin_regex=regex_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Setup gzip compression middleware
+# https://fastapi.tiangolo.com/advanced/middleware/#trustedhostmiddleware
+app.add_middleware(
+    GZipMiddleware,
+    # Messages below minimum_size (in bytes) will not be compressed
+    minimum_size=1_000_000,
+    # Compression level 1-9 (1 lowest compression, 9 highest compression)
+    compresslevel=1
+)
 
 @app.get("/")
 async def test():
@@ -13,4 +48,23 @@ async def test():
 async def thinking_of_a_number():
     return {
         "value": randint(1, 10)
+    }
+
+@app.get("/big-data1000")
+async def thinking_of_a_number2():
+    return {
+        "value": np.random.random((10, 10, 10)).flatten().tolist()
+    }
+
+@app.get("/big-data100000")
+async def thinking_of_a_number2():
+    return {
+        "value": np.random.random(100_000).flatten().tolist()
+    }
+
+# https://stackoverflow.com/questions/73921756/how-to-add-gzip-middleware-for-the-fastapi
+@app.get("/big-data1000000")
+async def thinking_of_a_number3():
+    return {
+        "value": np.random.random((100, 100, 100)).flatten().tolist()
     }
--- src/frontend/scripts/gymdash.js
+++ src/frontend/scripts/gymdash.js
@@ -1,1 +1,81 @@
-console.log("Hello World");
+
+
+const tryBtn        = document.querySelector("#try-api-btn");
+const tryBtnOut     = document.querySelector("#try-api-out");
+const testBtn       = document.querySelector("#test-api-btn");
+const testBtnOut    = document.querySelector("#test-api-out");
+
+
+function call_random() {
+    return fetch("http://127.0.0.1:8000/random");
+    // return fetch("http://127.0.0.1:8000/big-data1000");
+    // return fetch("http://127.0.0.1:8000/big-data100000");
+    // return fetch("http://127.0.0.1:8000/big-data1000000");
+}
+
+
+function displayNumberOutput(num) {
+    tryBtnOut.textContent = num;
+}
+function displayTestNumberOutput(outputs) {
+    testBtnOut.textContent = outputs;
+}
+
+function getNumber() {
+    return call_random()
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            // .json() returns a Promise with the result being the JSON info
+            return response.json();
+        })
+        .then((response) => {
+            displayNumberOutput(response.value);
+            return response;
+        })
+}
+
+function timeGetNumber() {
+    let start = Date.now();
+    return call_random()
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            let time = Date.now() - start;
+            console.log(response.value);
+            // console.log(`Got random response after ${time}ms`);
+            return Promise.resolve({time});
+        });
+}
+
+function testNumberAPI() {
+    const numRequests = 1000;
+    // Make n simultaneous API calls and time
+    const times = [];
+    const promises = [];
+    for (let i = 0; i < numRequests; i++) {
+        promises.push(
+            timeGetNumber().then((response) => {
+                times.push(response.time);
+                return response;
+            }).then((response) => {
+                // console.log(times);
+                displayTestNumberOutput(times);
+            })
+        );
+    }
+    Promise.all(promises).then((responses) => {
+        // Process all the response times.
+        const max = Math.max(...times);
+        const min = Math.min(...times);
+        const avg = times.reduce((sum, value) => {return sum + value;}) / times.length;
+        const metricString = `You requested ${numRequests} numbers. We measured ${times.length} times. Metrics (min, avg, max): (${min/1000}, ${avg/1000}, ${max/1000})s`;
+        console.log(metricString);
+        setTimeout(displayTestNumberOutput(metricString), 0);
+    });
+}
+
+tryBtn.addEventListener("click", getNumber);
+testBtn.addEventListener("click", testNumberAPI);
