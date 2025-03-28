058db5feb0185916e4f1d9ed8f17b6f8b8eb79ed
Chase Vickery <chasedvickery@gmail.com>
2025-03-27 22:19:58 -0400

    JS Module Attempt
    Attempted to refactor some JS code to a utility module. This failed
    (maybe) because the python http server was not allowing the module
    to load? Keeping as a reminder to get back to this.

---
+++ src/frontend/scripts/utils/usage.js
@@ -0,0 +1,25 @@
+// const resourceUsageUtils = (
+//     function() {
+//         const fetchResourceUsageDetailed    = () => {return fetch("http://127.0.0.1:8000/get-usage-detailed");}
+//         const fetchResourceUsageSimple      = () => {return fetch("http://127.0.0.1:8000/get-usage-simple");}
+
+//         const getResourceUsageDetailed = function() {
+//             return fetchResourceUsageDetailed()
+//                 .then((response) => {
+//                     return response.json();
+//                 });
+//         }
+
+//         const getResourceUsageSimple = function() {
+//             return fetchResourceUsageSimple()
+//                 .then((response) => {
+//                     return response.json();
+//                 });
+//         }
+
+//         return { getResourceUsageDetailed, getResourceUsageSimple };
+//     }
+// )();
+
+
+// export { resourceUsageUtils };
